f316539c218c7d342b2e1a0dcae4244c052510ef
Adrian Likins <alikins@redhat.com>
2018-07-07 17:50:46 -0400

    initial commit AlikinsLab

---
+++ README.md
@@ -0,0 +1,8 @@
+
+# AlikinsLab VCV Rack modules
+
+Modules for VCV Rack that are not really good or finished
+or general purpose.
+
+See https://github.com/alikins/Alikins-rack-plugins for
+more polished modules.
---
+++ src/AlikinsLab.cpp
@@ -0,0 +1,15 @@
+#include "AlikinsLab.hpp"
+
+
+Plugin *plugin;
+
+
+void init(Plugin *p) {
+	plugin = p;
+	p->slug = TOSTRING(SLUG);
+	p->version = TOSTRING(VERSION);
+
+	// Add all Models defined throughout the plugin
+	p->addModel(modelCableMadness);
+
+}
