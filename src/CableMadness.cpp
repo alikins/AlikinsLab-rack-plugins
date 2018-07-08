02a0a52cf7a6219b5be1d1c41389fe4b198a28b4
Adrian Likins <alikins@redhat.com>
2018-07-08 02:40:21 -0400

    better ranges for inputs

--- src/CableMadness.cpp
+++ src/CableMadness.cpp
@@ -18,16 +18,12 @@ struct CableMadness : Module {
 		NUM_LIGHTS
 	};
 
-	float tension = 0.5;
-	float opacity = 0.5;
+	// float tension = 0.5;
+	// float opacity = 0.5;
 
 	CableMadness() : Module(NUM_PARAMS, NUM_INPUTS, NUM_OUTPUTS, NUM_LIGHTS) {}
 	void step() override;
 
-	// For more advanced Module features, read Rack's engine.hpp header file
-	// - toJson, fromJson: serialization of internal data
-	// - onSampleRateChange: event triggered by a change of sample rate
-	// - onReset, onRandomize, onCreate, onDelete: implements special behavior when user clicks these from the context menu
 };
 
 
@@ -41,36 +37,36 @@ void CableMadness::step() {
         params[OPACITY_PARAM].value = inputs[OPACITY_INPUT].value;
     }
 
-	tension = params[TENSION_PARAM].value;
-	opacity = params[OPACITY_PARAM].value;
+	float in_tension = clamp(params[TENSION_PARAM].value, 0.0f, 10.0f);
+	float in_opacity = clamp(params[OPACITY_PARAM].value, 0.0f, 10.0f);
 
-	gToolbar->wireTensionSlider->setValue(tension);
-	gToolbar->wireOpacitySlider->setValue(opacity * 100.0f);
+	float tension = rescale(in_tension, 0.0f, 10.0f, 0.0f, 1.0f);
+	float opacity = rescale(in_opacity, 0.0f, 10.0f, 0.0f, 100.0f);
+
+	// Kind of overkill to do this at audio rate but this is
+	// a pretty ridiculous concept to start with...
+	gToolbar->wireTensionSlider->value = tension;
+	gToolbar->wireOpacitySlider->value = opacity;
 
 }
 
-
 struct CableMadnessWidget : ModuleWidget {
-	CableMadnessWidget(CableMadness *module) : ModuleWidget(module) {
-		setPanel(SVG::load(assetPlugin(plugin, "res/CableMadness.svg")));
-
-		addChild(Widget::create<ScrewSilver>(Vec(RACK_GRID_WIDTH, 0)));
-		addChild(Widget::create<ScrewSilver>(Vec(box.size.x - 2 * RACK_GRID_WIDTH, 0)));
-		addChild(Widget::create<ScrewSilver>(Vec(RACK_GRID_WIDTH, RACK_GRID_HEIGHT - RACK_GRID_WIDTH)));
-		addChild(Widget::create<ScrewSilver>(Vec(box.size.x - 2 * RACK_GRID_WIDTH, RACK_GRID_HEIGHT - RACK_GRID_WIDTH)));
-
-		addParam(ParamWidget::create<Davies1900hBlackKnob>(Vec(25, 95), module, CableMadness::TENSION_PARAM, 0.0f, 1.0f, 0.5));
-		addInput(Port::create<PJ301MPort>(Vec(29, 140), Port::INPUT, module, CableMadness::TENSION_INPUT));
-
-		addParam(ParamWidget::create<Davies1900hBlackKnob>(Vec(25, 220), module, CableMadness::OPACITY_PARAM, 0.0f, 1.0f, 0.5));
-		addInput(Port::create<PJ301MPort>(Vec(29, 265), Port::INPUT, module, CableMadness::OPACITY_INPUT));
-
-	}
+	CableMadnessWidget(CableMadness *module);
 };
 
+CableMadnessWidget::CableMadnessWidget(CableMadness *module) : ModuleWidget(module) {
+	setPanel(SVG::load(assetPlugin(plugin, "res/CableMadness.svg")));
 
-// Specify the Module and ModuleWidget subclass, human-readable
-// author name for categorization per plugin, module slug (should never
-// change), human-readable module name, and any number of tags
-// (found in `include/tags.hpp`) separated by commas.
+	addChild(Widget::create<ScrewSilver>(Vec(RACK_GRID_WIDTH, 0)));
+	addChild(Widget::create<ScrewSilver>(Vec(box.size.x - 2 * RACK_GRID_WIDTH, 0)));
+	addChild(Widget::create<ScrewSilver>(Vec(RACK_GRID_WIDTH, RACK_GRID_HEIGHT - RACK_GRID_WIDTH)));
+	addChild(Widget::create<ScrewSilver>(Vec(box.size.x - 2 * RACK_GRID_WIDTH, RACK_GRID_HEIGHT - RACK_GRID_WIDTH)));
+
+	addParam(ParamWidget::create<Davies1900hBlackKnob>(Vec(25, 95), module, CableMadness::TENSION_PARAM, 0.0f, 10.0f, 0.5));
+	addInput(Port::create<PJ301MPort>(Vec(29, 140), Port::INPUT, module, CableMadness::TENSION_INPUT));
+
+	addParam(ParamWidget::create<Davies1900hBlackKnob>(Vec(25, 220), module, CableMadness::OPACITY_PARAM, 0.0f, 10.0f, 0.5));
+	addInput(Port::create<PJ301MPort>(Vec(29, 265), Port::INPUT, module, CableMadness::OPACITY_INPUT));
+}
+
 Model *modelCableMadness = Model::create<CableMadness, CableMadnessWidget>("AlikinsLab", "CableMadness", "Cable Madness", OSCILLATOR_TAG);
